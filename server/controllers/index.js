5c67ad0a9c7d415cd05d59458ac1bc2d8e028078
Mark Harmon <neothej1sec@gmail.com>
2022-03-28 10:48:57 -0500

    added basic controller setup

---
+++ server/controllers/index.js
@@ -0,0 +1,43 @@
+const { Recipe, Comment } = require("../models")
+
+const addComment = async (req, res) => {
+    try {
+        
+        const comment = await new Comment(req.body)
+        await comment.save()
+        const Recipe = await Recipe.findByID(id)
+        return res.status(201).json({
+            comment
+        })
+    } catch (error) {
+        return res.status(500).json({error: error.message});
+    }
+}
+
+const getAllRecipes = async (req, res) => {
+    try {
+        const recipes = await Recipe.find({})
+        return res.status(200).json({ recipes })
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+const getRecipeById = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const recipe = await Recipe.findById(id)
+        if (recipe) {
+            return res.status(200).json({ recipe });
+        }
+        return res.status(404).send('Recipe with the specified ID does not exists');
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+module.exports = {
+    addComment,
+    getAllRecipes,
+    getRecipeById
+}
