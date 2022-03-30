450d3070f7641f8367bb61a48dbc61251e5b671f
Mark Harmon <neothej1sec@gmail.com>
2022-03-30 13:14:23 -0500

    finally accessed data and have recipeResults updated in use state

--- client/personal-recipe-book/src/pages/Main.js
+++ client/personal-recipe-book/src/pages/Main.js
@@ -11,29 +11,46 @@ const Main = () => {
     const [recipeResults, setRecipeResults] = useState([])
 
 
+    let theArray = [];
     
+    // useEffect(() => {
+    //     axios
+    //         .get(`http://localhost:3001/api/recipes`, { mode: 'cors' } )
+            
+    //         .then(res => res.json())
+    //         .then((result) => {
+    //             theArray[0].id = result.id;
+    //             theArray[0].name = result.name;
+    //             theArray.push({
+    //                 id: result.id,
+    //                 name: result.name,
+    //             })
+    //         })
+    //         console.log(theArray)
+    // }, []);
+    
+    // for(let i=0; i<=recipes.data.recipes.length; i++){
+    //     setRecipeResults(recipes.data.recipes[i])}
    
     // use effect to make the axios call
     useEffect(() => {
     const getRecipeResults = async () => {
         const recipes = await axios.get("http://localhost:3001/api/recipes", { mode: 'cors' } )
-        for(let i=0; i<=recipes.data.recipes.length; i++){
-        setRecipeResults(recipes.data.recipes[i])
-        
-        
-        }
+        const data = [...recipes.data.recipes]
+        setRecipeResults(data)
+        console.log(recipeResults)
     }
     
     getRecipeResults()
     },[])
     // const navigate = useNavigate()
-    console.log(recipeResults)
+    //console.log(recipeResults)
     // const showRecipe = () => {
     //     navigate(recipes.data._id)
     // }
 
     // const allRecipes = [...recipes.data.recipes]
-    // console.log(allRecipes)
+    
 
     // for(let i=0; i<recipeArray.length; i++){
     //     allRecipes[i].map((recipe) => ( ))}
@@ -43,13 +60,13 @@ const Main = () => {
             <Search />
             <div className="recipeSearch">
                 <h3>Recipe List</h3>
-                   {recipeResults.map((recipe) => (
+                   {/* {recipeResults.map((recipe) => ( */}
                     <Recipe 
-                        name={recipe.data.recipes}
-                        key={recipe.key}
-                        ingredients={recipe.ingredients}
+                        // name={recipe.data.recipes.name}
+                        // key={recipe.key}
+                        // ingredients={recipe.ingredients}
                     />
-                    ))}
+                    {/* ))} */}
             </div>
             </div>
         </div>
