3359c3000cdd05aac61b8eb7c90b7ff3c69d2616
Mark Harmon <neothej1sec@gmail.com>
2022-03-30 13:35:49 -0500

    working on maping check conditional rendering for help

--- client/personal-recipe-book/src/pages/Main.js
+++ client/personal-recipe-book/src/pages/Main.js
@@ -11,24 +11,6 @@ const Main = () => {
     const [recipeResults, setRecipeResults] = useState([])
 
 
-    let theArray = [];
-    
-    // useEffect(() => {
-    //     axios
-    //         .get(`http://localhost:3001/api/recipes`, { mode: 'cors' } )
-            
-    //         .then(res => res.json())
-    //         .then((result) => {
-    //             theArray[0].id = result.id;
-    //             theArray[0].name = result.name;
-    //             theArray.push({
-    //                 id: result.id,
-    //                 name: result.name,
-    //             })
-    //         })
-    //         console.log(theArray)
-    // }, []);
-    
     // for(let i=0; i<=recipes.data.recipes.length; i++){
     //     setRecipeResults(recipes.data.recipes[i])}
    
@@ -40,17 +22,15 @@ const Main = () => {
         setRecipeResults(data)
         console.log(recipeResults)
     }
-    
     getRecipeResults()
     },[])
     // const navigate = useNavigate()
-    //console.log(recipeResults)
     // const showRecipe = () => {
     //     navigate(recipes.data._id)
     // }
 
-    // const allRecipes = [...recipes.data.recipes]
-    
+    // u2w1ThurLessConRendering for info
+    //const handleSearchClick = () => showRecipe(onSubmit1)
 
     // for(let i=0; i<recipeArray.length; i++){
     //     allRecipes[i].map((recipe) => ( ))}
@@ -60,13 +40,13 @@ const Main = () => {
             <Search />
             <div className="recipeSearch">
                 <h3>Recipe List</h3>
-                   {/* {recipeResults.map((recipe) => ( */}
-                    <Recipe 
-                        // name={recipe.data.recipes.name}
-                        // key={recipe.key}
-                        // ingredients={recipe.ingredients}
-                    />
-                    {/* ))} */}
+                    {recipeResults.map((recipe) => (
+                        <Recipe 
+                            name={recipe.name}
+                            key={recipe.key}
+                            ingredients={recipe.ingredients}
+                        />
+                    ))}
             </div>
             </div>
         </div>
