4aa7baa59e536ae11bd3ff6dea834215f0522479
Mark Harmon <neothej1sec@gmail.com>
2022-03-30 21:51:48 -0500

    git issue, have recipes rending on screen now

--- client/personal-recipe-book/src/pages/About.js
+++ client/personal-recipe-book/src/pages/About.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 const About = () => {
     return (
         <div className="About">
-            <h1>About</h1>
+            <h1>About:</h1>
             <p>My idea for this project was to create a place to store and save personal recipes for future use.</p>
             <p>Not just search your average google recipes, I mean the ones you're family/friends passed to you. </p>
             <p>The type of recipes you call you're family recipes, the recipes that need to be perserved.</p>
