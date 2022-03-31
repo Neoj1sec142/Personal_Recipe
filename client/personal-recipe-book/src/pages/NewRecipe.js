a6af4e5e4a69a159a2d8895ec5a64547b8669589
Mark Harmon <neothej1sec@gmail.com>
2022-03-30 22:43:51 -0500

    got search bars added to new recipe page need to set searchbar functionality up

--- client/personal-recipe-book/src/pages/NewRecipe.js
+++ client/personal-recipe-book/src/pages/NewRecipe.js
@@ -1,9 +1,36 @@
-
+//import { useNavigate } from 'react-router-dom'
 
 const NewRecipe = () => {
+    // const navigate = useNavigate();
+    // const showRecipe = () => {
+    //     navigate(recipeResults.data._id)
+    // }
+    // u2w1ThurLessConRendering for info
+    const handleChange = () => {}
     return(
-        <div></div>
+        <div className='new'>
+            <form>
+                <input name='new_name' value='name' placeholder='name' onChange={handleChange} />
+                <input name='new_type' value='type' placeholder='name' />
+                <input name='new_ingredients' value='ingredients' placeholder='ingredients' />
+                <input name='new_servings' value='servings' placeholder='servings' />
+                <input name='new_time_cook' value='time_cook' placeholder='time_cook' />
+                <input name='new_temp' value='temp' placeholder='temp' />
+                <input name='new_caution' value='caution' placeholder='caution' />
+                <input name='new_steps' value='steps' placeholder='steps' />
+                
+            </form>
+        </div>
     )
 }
 
 export default NewRecipe
+
+    // name: "",
+    // type: '',
+    // ingredients: "",
+    // servings: "",
+    // time_cook: "",
+    // temp: "",
+    // caution: "",
+    // steps: ""
